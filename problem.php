9cabc38082ff4979042ae2ec2cb70d87e7261a64
Pizzaroot <therealpizzaroot@gmail.com>
2020-12-27 23:44:57 +0900

    Update problem.php

--- problem.php
+++ problem.php
@@ -104,7 +104,7 @@ and is wrapped around the whole page content, except for the footer in this exam
         return $ipaddr1ess;
     }
     
-    $sql = "SELECT * FROM problems WHERE id=$id AND code=''";
+    $sql = "SELECT * FROM problems WHERE id=$id";
     $result = mysqli_query($conn, $sql) or die('?ㅋㅋ');
     if (mysqli_num_rows($result) == 0)
     {
