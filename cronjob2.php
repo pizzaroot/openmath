8c6234d5d6e68e1843c39ce18022a0c85fbeea1f
Pizzaroot <therealpizzaroot@gmail.com>
2020-12-28 16:04:37 +0900

    s

--- cronjob2.php
+++ cronjob2.php
@@ -12,6 +12,7 @@ while ($row2 = mysqli_fetch_assoc($result))
 	$rid = $row2['id'];
 	$save = $row2['save'];
 	$savedata = explode('|',$save);
+	//first
 	$max = 0;
 	for ($i=1;$i<count($savedata);$i++) {
 		$max = max($max, intval(explode(',', $savedata[$i])[1]));
@@ -19,10 +20,39 @@ while ($row2 = mysqli_fetch_assoc($result))
 	$first="";
 	for ($i=1;$i<count($savedata);$i++) {
 		if ($max == intval(explode(',', $savedata[$i])[1])) {
-			$first=$max."점~|~".explode(',', $savedata[$i])[0];
+			$first=explode(',', $savedata[$i])[1]."점~|~".explode(',', $savedata[$i])[0];
+			array_splice($savedata, $i, 1);
+			break;
 		}
 	}
-	$sql = "UPDATE problems SET first='$first' WHERE id=$rid";
+	//second
+	$max = 0;
+	for ($i=1;$i<count($savedata);$i++) {
+		$max = max($max, intval(explode(',', $savedata[$i])[1]));
+	}
+	$second="";
+	for ($i=1;$i<count($savedata);$i++) {
+		if ($max == intval(explode(',', $savedata[$i])[1])) {
+			$second=explode(',', $savedata[$i])[1]."점~|~".explode(',', $savedata[$i])[0];
+			array_splice($savedata, $i, 1);
+			break;
+		}
+	}
+	//third
+	$max = 0;
+	for ($i=1;$i<count($savedata);$i++) {
+		$max = max($max, intval(explode(',', $savedata[$i])[1]));
+	}
+	$third="";
+	for ($i=1;$i<count($savedata);$i++) {
+		if ($max == intval(explode(',', $savedata[$i])[1])) {
+			$third=explode(',', $savedata[$i])[1]."점~|~".explode(',', $savedata[$i])[0];
+			array_splice($savedata, $i, 1);
+			break;
+		}
+	}
+	
+	$sql = "UPDATE problems SET first='$first', second='$second', third='$third' WHERE id=$rid";
 	mysqli_query($conn, $sql) or die('?ㅋㅋ');
 }
 ?>
