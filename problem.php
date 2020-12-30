acbbfed21e15f6c094eb7a9cbdcb14ab4d906ca9
Pizzaroot <therealpizzaroot@gmail.com>
2020-12-30 21:15:15 +0900

    Add files via upload

--- problem.php
+++ problem.php
@@ -504,28 +504,40 @@ if ($codepath == "") {
                 }
             }
 } else if ($setvarkthx == 12230) {
-	include_once "api.php";
-	$execresult = executeCode($codepath, $_GET['answer'], $featuredval);
-	if ($execresult["statusCode"]==200) {
-		$execscore = trim($execresult["output"]);
-		if (intval($execscore) > 0 && strpos($codesave, "|".$naver_user_id.",".$execscore) === false) {
-			$codesave .= "|".$naver_user_id.",".$execscore;
-			$sql9 = "UPDATE problems SET save='$codesave' WHERE id=$id";
-			$result = mysqli_query($conn, $sql9) or die('?ㅋㅋ루e');			
-		}
-		if (intval($execscore) > 0) {
-			echo $execscore."점<br>";
-		} else if (is_numeric($execscore)) {
-			echo "0점<br>";
-		} else {
-			include_once "error.php";
-		}
-	} else {
-		
+	
+	$sql49 = "SELECT * FROM users WHERE nid=$naver_user_id";
+    $result49 = mysqli_query($conn, $sql49) or die('ㄹㅇㅋㅋ');
+	
+	while ($row = mysqli_fetch_assoc($result49))
+	{
+		$lastjudged=$row['lastjudge'];
 	}
-	$sql10 = "UPDATE users SET lastjudge=".time()." WHERE nid=$naver_user_id";
-	$result = mysqli_query($conn, $sql10) or die('?ㅋㅋe3루');
-	include_once "cronjob2.php";
+	if (time()-$lastjudged < 60) {
+		echo (60-time()+$lastjudged)."초 후에 다시 시도하세요.<br>";
+	} else {
+		include_once "api.php";
+		$execresult = executeCode($codepath, $_GET['answer'], $featuredval);
+		if ($execresult["statusCode"]==200) {
+			$execscore = trim($execresult["output"]);
+			if (intval($execscore) > 0 && strpos($codesave, "|".$naver_user_id.",".$execscore) === false) {
+				$codesave .= "|".$naver_user_id.",".$execscore;
+				$sql9 = "UPDATE problems SET save='$codesave' WHERE id=$id";
+				$result = mysqli_query($conn, $sql9) or die('?ㅋㅋ루e');			
+			}
+			if (intval($execscore) > 0) {
+				echo $execscore."점<br>";
+			} else if (is_numeric($execscore)) {
+				echo "0점<br>";
+			} else {
+				include_once "error.php";
+			}
+		} else {
+			
+		}
+		$sql10 = "UPDATE users SET lastjudge=".time()." WHERE nid=$naver_user_id";
+		$result = mysqli_query($conn, $sql10) or die('?ㅋㅋe3루');
+		include_once "cronjob2.php";
+	}
 }
         }
     }
